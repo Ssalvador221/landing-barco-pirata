c18669b645ca1337b3a2907fda80f17a8087b636
Ssalvador221 <j8731615@gmail.com>
2024-11-27 17:27:47 -0300

    fix: favicon and who we are text

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -35,25 +35,39 @@ function History() {
             <CarouselPrevious />
             <CarouselNext className="max-sm:hidden" />
           </Carousel>
-          <Card className="bg-[#e6d2ae] w-[400px] border-[#8b4513]">
+          <Card className="bg-[#e6d2ae] w-[700px] border-[#8b4513]">
             <CardHeader>
               <CardTitle className="text-[#8b4513] flex items-center">
                 <Users className="w-6 h-6 mr-2" />
-                Who We Are
+                Quem somos
               </CardTitle>
             </CardHeader>
             <CardContent className="text-[#5e2c04]">
+              <p>
+                O sonho começou bem pequenininho na praia de{' '}
+                <strong>Ponta das Canas</strong>, por volta de 1998.
+              </p>
+              <br />
               <p className="mb-4">
-                We are a crew of passionate maritime enthusiasts and history
-                buffs, dedicated to bringing the golden age of piracy to life.
-                Our team consists of experienced sailors, knowledgeable
-                historians, and adventure-seeking guides.
+                Durante o inverno, a embarcação “boca aberta” garantia o
+                sustento da família com a pesca e quando chegava o verão, eram
+                feitas adaptações, como colocação de um guarda sol para ser
+                utilizados nos passeios.
               </p>
               <p>
-                Founded in 2010, our mission is to provide an immersive and
-                educational experience that combines the thrill of sailing with
-                the rich history of piracy in these waters. We strive to create
-                unforgettable memories for all who board The Golden Horizon.
+                A procura dos turistas para visitar a Ilha do Francês e
+                Anhatomirim era muito grande e a capacidade máxima dessa pequena
+                embarcação era de apenas 12 passageiros e sem projetar, esse foi
+                o início.
+              </p>
+              <br />
+              <p>
+                O Pérola Negra foi construído em 2007, uma escuna pirata com
+                capacidade para 200 passageiros, com embarque em Canasvieiras,
+                leva alegria e muita diversão, com atrações inesquecíveis, show
+                de piratas e teatro a bordo, considerado o melhor e mais
+                divertido barco pirata de Florianópolis. Encantar e divertir, é
+                a nossa missão.
               </p>
             </CardContent>
           </Card>
